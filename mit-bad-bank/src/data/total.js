307b2d9f98d0c71cf94233d44a912aed663f4739
OneCleverFox <124699699+OneCleverFox@users.noreply.github.com>
2023-07-28 07:38:38 +0200

    modified:   mit-bad-bank/public/favicon.ico 	new file:   mit-bad-bank/public/fox-logo.ico

--- mit-bad-bank/src/data/total.js
+++ mit-bad-bank/src/data/total.js
@@ -1,21 +1,25 @@
 // Function to calculate the total amount for a given target type in the data array.
 const getTotal = (targetType, data) => {
-    // If the data array is empty, return 0.
-    if (data.length === 0) return 0;
-  
-    // Filter the data array to get an array containing only the records with the target type.
-    const targetArray = data.filter((record) => record.type === targetType);
-  
-    // If no records with the target type are found, return 0.
-    if (targetArray.length === 0) return 0;
-  
-    // If only one record with the target type is found, return its amount.
-    if (targetArray.length === 1) return targetArray[0].amount;
-  
-    // If multiple records with the target type are found, calculate the total amount using reduce.
-    return targetArray.reduce((acc, obj) => acc + obj.amount, 0);
-  };
-  
-  // Export the getTotal function 
-  export default getTotal;
-  
+  // If the data array is empty, return 0.
+  if (data.length === 0) return "0.00";
+
+  // Filter the data array to get an array containing only the records with the target type.
+  const targetArray = data.filter((record) => record.type === targetType);
+
+  // If no records with the target type are found, return 0.
+  if (targetArray.length === 0) return "0.00";
+
+  // If only one record with the target type is found, return its amount.
+  if (targetArray.length === 1) {
+    const amount = targetArray[0].amount.toFixed(2);
+    return amount.padEnd(amount.length + 1, "0"); // Add a "0" if there's only one decimal place.
+  }
+
+  // If multiple records with the target type are found, calculate the total amount using reduce.
+  const totalAmount = targetArray.reduce((acc, obj) => acc + obj.amount, 0);
+  const formattedTotal = totalAmount.toFixed(2);
+  return formattedTotal;
+};
+
+// Export the getTotal function
+export default getTotal;
